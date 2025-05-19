15e741bb4250cd11fa9b48d8922f9f16a74f297f
Andreea Salca <andreea.salca@MacBook-Pro.local>
2025-05-19 12:25:31 +0100

    feat: add meals and community pages

--- foodies/app/page.js
+++ foodies/app/page.js
@@ -1,20 +1,24 @@
 import Link from "next/link";
 
+import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
+
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share Meals</Link>
-      </p>
-      <p>
-        <Link href="/community">Community</Link>
-      </p>
-    </main>
+    <header className={classes.header}>
+      <div className={classes.slideshow}>
+        <ImageSlideshow />
+      </div>
+      <div>
+        <div className={classes.hero}>
+          <h1>NextLevel Food for NextLevel Foodies</h1>
+          <p> Taste & share food from all over the world.</p>
+        </div>
+        <div className={classes.cta}>
+          <Link href="/community">Join the Community</Link>
+          <Link href="/meals">Explore Meals</Link>
+        </div>
+      </div>
+    </header>
   );
 }
--- next-essential/page.js
+++
@@ -1,7 +0,0 @@
-export default function AwesomePage() {
-  return (
-    <main>
-      <h1>Hey</h1>
-    </main>
-  );
-}
