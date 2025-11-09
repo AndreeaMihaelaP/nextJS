d4a553831347babadbf233625b8dd86c49ed9883
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2025-11-09 17:10:58 +0000

    feat(share): add share functionality

--- foodies/app/meals/share/page.js
+++ foodies/app/meals/share/page.js
@@ -1,4 +1,7 @@
+"use client";
+
 import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/lib/actions";
 
 import classes from "./page.module.css";
 
@@ -12,7 +15,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -37,10 +40,9 @@ export default function ShareMealPage() {
               id="instructions"
               name="instructions"
               rows="10"
-              required
-            ></textarea>
+              required></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
